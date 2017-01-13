2600c743f6246c4b0fdf53135e7446d62bdf7a49
Allan de Almeida Vigiano <allanvigiano@gmail.com>
2017-01-13 18:41:29 -0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# Vue-section07-Exercise7
-Section 7 of Maximilian Schwarzmüller course posted on Udemy (https://www.udemy.com/vuejs-2-the-complete-guide)
+# vue-cli
+
+> A Vue.js project
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+
+        <app-header></app-header>
+
+        <div class="row">
+
+            <app-servers></app-servers>
+
+            <app-server-details></app-server-details>
+        </div>
+
+        <app-footer></app-footer>
+
+    </div>
+</template>
+
+<script>
+    import Header from './Header.vue';
+    import Footer from './Footer.vue';
+    import Servers from './Servers.vue';
+    import ServerDetails from './ServerDetails.vue';
+
+    export default {
+        data: function () {
+            return {
+
+            };
+        },
+        components: {
+            'app-header': Header,
+            'app-footer': Footer,
+            'app-servers': Servers,
+            'app-server-details': ServerDetails,
+        }
+    }
+</script>
+
+<style>
+
+</style>
